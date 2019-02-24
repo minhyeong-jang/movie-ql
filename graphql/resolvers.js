5fcae2134b7e5ca16ba762e0fafc65200e3b4c57
Noah <savedream75@gmail.com>
2019-02-25 01:50:08 +0900

    Add Test Schema

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+const resolvers = {
+  Query: {
+    name: () => "Minhyeong"
+  }
+};
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-
+  typeDefs: "graphql/schema.graphql",
+  resolvers
 });
 
-server.start(() => console.log("Server Start!"));
+server.start(() => console.log("Server Start -> localhost:3000"));
