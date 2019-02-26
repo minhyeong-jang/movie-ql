ee555137c927e44d7bed34b92d0dd659bc927f58
Noah <savedream75@gmail.com>
2019-02-26 16:30:41 +0900

    Add Mutation DeleteMovie

--- graphql/db.js
+++ graphql/db.js
@@ -34,7 +34,7 @@ export const getById = id => {
 }
 
 export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id === id);
+  const cleanedMovies = movies.filter(movie => movie.id !== id);
   if (movies.length > cleanedMovies.length) {
     movies = cleanedMovies;
     return true;
