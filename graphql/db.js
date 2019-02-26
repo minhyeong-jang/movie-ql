cacce367c40706bccb0119325fc8175ef534da88
Noah <savedream75@gmail.com>
2019-02-26 14:36:04 +0900

    Update Data to Movie

--- graphql/db.js
+++ graphql/db.js
@@ -1,37 +1,44 @@
-export const people = [
+export let movies = [
   {
     id: 0,
-    name: "Minhyeong",
-    age: 23,
-    gender: "male"
+    name: "Movie 0",
+    score: 23,
   },
   {
     id: 1,
-    name: "Test1",
-    age: 22,
-    gender: "male"
+    name: "Movie 1",
+    score: 99,
   },
   {
     id: 2,
-    name: "Test2",
-    age: 22,
-    gender: "male"
+    name: "Movie 2",
+    score: 5,
   },
   {
     id: 3,
-    name: "Test3",
-    age: 22,
-    gender: "male"
+    name: "Movie 3",
+    score: 23,
   },
   {
     id: 4,
-    name: "Test4",
-    age: 22,
-    gender: "male"
+    name: "Movie 4",
+    score: 52,
   }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter(movie => movie.id === id);
+  return filteredMovies[0];
+}
+
+export const deleteMovie = id => {
+  const cleanedMovies = movies.filter(movie => movie.id === id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 }
